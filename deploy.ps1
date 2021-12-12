12f00b2676a366882d607830c258337ddddc6683
Luka Robajac <luka.robajac@gmail.com>
2021-12-12 17:41:42 +0100

    Updated powershell script

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,36 @@
-param ($m)
+param ([Parameter(Mandatory)]$m, $dryRun, $suppressGitOutput)
 
-Write-Output "Building resources for demo website..."
+function PrintMessage {
+    param (
+        [Parameter(Mandatory)]$message,
+        $foregroundColor="Green"
+    )
+
+    $params = @{ ForeGroundColor=$foregroundColor }
+    
+    Write-Host $($message) @params
+}
+
+$isDryRun = $( if(! $null -eq $dryRun){ $TRUE }else { $FALSE } )
+
+$dryRunPrepend = $( if($isDryRun){ '(dry run) ' } )
+$gitAdd = $( if($isDryRun){ 'git add . --dry-run' }else{ 'git add .' }  )
+$gitCommit = $( if($isDryRun){ 'git commit --dry-run -m "' + $m + '"' }else{ 'git commit -m "' + $m + '"' }  )
+$gitPush = $( if($isDryRun){ 'git push --dry-run' }else{ 'git push' }  )
+
+PrintMessage "Building resources for demo website..."
 npm run demo-build --quiet >$null
-Write-Output "Building main module..."
+
+PrintMessage "Building main module..."
 npm run build --quiet >$null
-Write-Output "Staging files for commit to the repository..."
-git add . >$null
-Write-Output "Comitting files to the repository..."
-git commit -m "$($m)"
-Write-Output "Pushing to the repository..."
-git push
-Write-Output "Build successfully deployed!"
+
+PrintMessage "$($dryRunPrepend)Staging files for commit to the repository..."
+Invoke-Expression $gitAdd >$null
+
+PrintMessage "$($dryRunPrepend)Comitting files to the repository..."
+Invoke-Expression $gitCommit >$null
+
+PrintMessage "$($dryRunPrepend)Pushing to the repository..."
+Invoke-Expression $gitPush >$null
+
+PrintMessage "Build successfully deployed!"
