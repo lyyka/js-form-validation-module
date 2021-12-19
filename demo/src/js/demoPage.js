551e758b1ed11f066ad82b08a519f1cc5890b706
Luka Robajac <luka.robajac@gmail.com>
2021-12-19 14:08:10 +0100

    New checkboxes and live validation control implemented on demo page

--- demo/src/js/demoPage.js
+++ demo/src/js/demoPage.js
@@ -5,7 +5,16 @@ import {forms} from './components/forms';
 forms(rules).forEach(formObject => {
     const form = document.querySelector(`#${formObject.formId}`);
     form.querySelector('button').onclick = function() {
-        const {formIsValid} = validateForm(form, formObject.validationRules);
+
+        const options = {
+            live: form.querySelector('input[name=live]').checked,
+        };
+
+        const {formIsValid} = validateForm(
+            form, 
+            formObject.validationRules,
+            options
+        );
 
         formObject.callback(form, formIsValid);
     };
