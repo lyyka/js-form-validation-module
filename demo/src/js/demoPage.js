8fc22a6d24519f80d196ae6304450656c362a9ca
Luka Robajac <luka.robajac@gmail.com>
2021-12-19 20:45:05 +0100

    Rules search implemented on demo page

--- demo/src/js/demoPage.js
+++ demo/src/js/demoPage.js
@@ -2,6 +2,28 @@ import '../scss/_global.scss';
 import {Form,rules} from '../../../dist/index.js';
 import {forms} from './components/forms';
 
+const searchField = document.querySelector('#search');
+searchField.addEventListener('input', () => {
+    const value = searchField.value;
+    const allForms = document.querySelectorAll('form');
+    console.clear();
+    allForms.forEach(form => {
+        const parent = form.parentNode;
+        const section = parent.parentNode;
+        const searchables = parent.querySelectorAll('[data-searchable]');
+        for(let i = 0; i < searchables.length; i++) {
+            const text = searchables[i].innerText.toLowerCase().trim();
+            const searchString = value.toLowerCase().trim();
+            if(text.includes(searchString) || !value) {
+                section.style.display = 'block';
+                break;
+            } else {
+                section.style.display = 'none';
+            }
+        }
+    });
+});
+
 forms(rules).forEach(formObject => {
     const form = document.querySelector(`#${formObject.formId}`);
     const isLiveCheck = form.querySelector('input[name=live]');
